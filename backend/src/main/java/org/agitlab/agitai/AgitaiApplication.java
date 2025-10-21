1e100b6298fb57ae647a1d73c8dfb48c8b3057d9
경찬 <chan@gyeongchan-ui-MacBookPro.local>
2025-10-21 15:30:15 +0900

    initial project version

---
+++ backend/src/main/java/org/agitlab/agitai/AgitaiApplication.java
@@ -0,0 +1,13 @@
+package org.agitlab.agitai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgitaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgitaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/agitlab/agitai/AgitaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agitlab.agitai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgitaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
